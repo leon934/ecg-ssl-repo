8fa6ea6b227f9e8259e20c45481aac8f4ce9d7f5
Leon Nguyen <leonlnguyen93@gmail.com>
2026-01-02 22:34:22 -0600

    Fleshed out training pipeline for SimCLR implementation with our specific dataset.

---
+++ echo_simclr/run.py
@@ -0,0 +1,81 @@
+import argparse
+import logging
+from pathlib import Path
+import os
+
+import torch
+
+from model import ViTModel, vit_dict
+from data_aug.cl_dataset import ContrastiveLearningDataset
+from simclr import SimCLR
+
+parser = argparse.ArgumentParser(description='PyTorch SimCLR')
+
+parser.add_argument('-a', '--arch', metavar='ARCH',
+                    choices=vit_dict,
+                    help='model architecture: ' +
+                         ' | '.join(vit_dict))
+parser.add_argument('--lr', '--learning-rate', default=0.0003, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-dataset-name', default='echonet-dynamic',
+                    help='dataset name', choices=['echonet-dynamic'])
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--disable-cuda', action='store_true',
+                    help='Disable CUDA')
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--log-every-n-steps', default=100, type=int,
+                    help='Log every n steps')
+parser.add_argument('--temperature', default=0.07, type=float,
+                    help='softmax temperature (default: 0.07)')
+
+def setup_logging(log_dir: Path):
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    logging.basicConfig(
+        filename=log_dir / "training.log",
+        level=logging.DEBUG,
+        filemode="w"
+    )
+
+def main():
+    args = parser.parse_args()
+
+    args.device = (
+        f"cuda:{args.gpu_index}"
+        if torch.cuda.is_available() and args.gpu_index is not None and args.gpu_index >= 0
+        else "cpu"
+    )
+    args.workers = max(1, os.cpu_count() // 2)
+
+    data = ContrastiveLearningDataset(args.data)
+    dataset = data.get_dataset(args.dataset_name)
+
+    train_data = dataset["TRAIN"]
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True, drop_last=True
+    )
+
+    model = ViTModel(base_model=args.arch)
+    model = model.to(args.device)
+    
+    optimizer = torch.optim.Adam(model.parameters(), args.lr, weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader), eta_min=0, 
+                                                           last_epoch=-1)
+    
+    simclr_model = SimCLR(model=model, optimizer=optimizer, scheduler=scheduler, args=args)
+    simclr_model.train(train_loader)
+
+if __name__ == "__main__":
+    setup_logging(Path("./logs"))
+    main()
---
+++ echo_simclr/simclr.py
@@ -0,0 +1,98 @@
+import logging
+import os
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from utils import accuracy, save_checkpoint, save_config_file
+
+torch.manual_seed(0)
+
+class SimCLR(object):
+    def __init__(self, model, optimizer, scheduler, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.args = args
+
+        self.writer = SummaryWriter()
+        self.criterion = torch.nn.CrossEntropyLoss().to(self.args.device)
+
+    def info_nce_loss(self, features):
+        labels = torch.cat([torch.arange(self.args.batch_size) for _ in range(2)], dim=0)
+        labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+        labels = labels.to(self.args.device)
+
+        features = F.normalize(features, dim=1)
+
+        similarity_matrix = torch.matmul(features, features.T)
+        # assert similarity_matrix.shape == (
+        #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
+        # assert similarity_matrix.shape == labels.shape
+
+        # discard the main diagonal from both: labels and similarities matrix
+        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.args.device)
+        labels = labels[~mask].view(labels.shape[0], -1)
+        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+        # assert similarity_matrix.shape == labels.shape
+
+        # select and combine multiple positives
+        positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+
+        # select only the negatives the negatives
+        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+
+        logits = torch.cat([positives, negatives], dim=1)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.args.device)
+
+        logits = logits / self.args.temperature
+        return logits, labels
+
+    def train(self, train_loader):
+        save_config_file(self.writer.log_dir, self.args)
+
+        n_iter = 0
+        logging.info(f"Start SimCLR training for {self.args.epochs} epochs.")
+        logging.info(f"Training with gpu: {self.args.disable_cuda}.")
+
+        for curr_epoch in range(self.args.epochs):
+            for images, _ in tqdm(train_loader):
+                images = torch.cat(images, dim=0).to(self.args.device)
+
+                features = self.model(images)
+                logits, labels = self.info_nce_loss(features)
+
+                loss = self.criterion(logits, labels)
+            
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            if n_iter % self.args.log_every_n_steps == 0:
+                top1, top5 = accuracy(logits, labels, topk=(1, 5))
+
+                self.writer.add_scalar('loss', loss, global_step=n_iter)
+                self.writer.add_scalar('acc/top1', top1[0], global_step=n_iter)
+                self.writer.add_scalar('acc/top5', top5[0], global_step=n_iter)
+                self.writer.add_scalar('learning_rate', self.scheduler.get_lr()[0], global_step=n_iter)
+            
+            n_iter += 1
+
+            if curr_epoch >= 10:
+                self.scheduler.step()
+
+            logging.debug(f"Epoch: {curr_epoch}\tLoss: {loss}\tTop1 accuracy: {top1[0]}")
+
+        logging.info("Training has finished.")
+        # save model checkpoints
+        checkpoint_name = 'checkpoint_{:04d}.pth.tar'.format(self.args.epochs)
+        
+        save_checkpoint({
+            'epoch': self.args.epochs,
+            'arch': self.args.arch,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
+        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
